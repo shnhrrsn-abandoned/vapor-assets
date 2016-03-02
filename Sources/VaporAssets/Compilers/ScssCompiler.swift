80d2d51ea3c39df328455e39163b130f07b929be
shnhrrsn <shnhrrsn@gmail.com>
2016-03-01 22:18:55 -0500

    ScssCompiler now uses node-sass

--- Sources/VaporAssets/Compilers/ScssCompiler.swift
+++ Sources/VaporAssets/Compilers/ScssCompiler.swift
@@ -8,30 +8,15 @@
 import Foundation
 
 public class ScssCompiler: TaskCompiler {
-	var bin: String?
-
-	public required init(shouldMinify: Bool) {
-		super.init(shouldMinify: shouldMinify)
-
-		self.bin = NSTask.execute("/usr/bin/which", arguments: [ "scss" ])?.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-
-		if self.bin?.characters.count == 0 {
-			self.bin = nil
-		}
-	}
 
 	public override func getCompilationTask(path: String, context: AnyObject? = nil) -> NSTask {
-		guard let bin = self.bin else {
-			fatalError("\(self.dynamicType) is not supported.")
-		}
-
 		let minify = context as? String ?? (self.shouldMinify ? "compressed" : "nested")
 
 		let task = NSTask()
-		task.launchPath = bin
+		task.launchPath = "/usr/bin/env"
 		task.arguments = [
-			"-t", minify,
-			"--compass",
+			"node-sass",
+			"--output-style", minify,
 			"--precision=14",
 			path
 		]
