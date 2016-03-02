e2f38fd16860c5a2901d96b9c453724a8cdc9447
shnhrrsn <shnhrrsn@gmail.com>
2016-03-01 23:25:19 -0500

    Added caching

--- Sources/VaporAssets/Controllers/Controller.swift
+++ Sources/VaporAssets/Controllers/Controller.swift
@@ -11,6 +11,7 @@ import Foundation
 private typealias RequestInfo = (path: String, fileExtension: String)
 
 class Controller: Vapor.Controller {
+	private let cache = NSCache()
 
 	required init() { }
 
@@ -18,7 +19,7 @@ class Controller: Vapor.Controller {
 		let info = self.info(request)
 
 		guard let asset = Asset(path: info.path) else {
-			return "Error"
+			return Response(status: .Error, text: "Unsupported asset")
 		}
 
 		return self.process(info, contentType: asset.mime, asset: asset, lastModified: asset.getLastModified())
@@ -31,17 +32,29 @@ class Controller: Vapor.Controller {
 			return try! String(contentsOfFile: info.path)
 		}
 
-		do {
-			if let compiled = try asset.compile() {
-				return Response(status: .OK, data: compiled.utf8, contentType: .Other(asset.mime))
-			} else {
-				throw CompilationError.UnknownError
+		let cacheKey = info.path + String(lastModified)
+		let response: Response
+
+		if let contents = self.cache.objectForKey(cacheKey) as? String {
+			response = Response(status: .OK, data: contents.utf8, contentType: .Other(asset.mime))
+		} else {
+			do {
+				if let compiled = try asset.compile() {
+					self.cache.setObject(compiled, forKey: cacheKey)
+					response = Response(status: .OK, data: compiled.utf8, contentType: .Other(asset.mime))
+				} else {
+					throw CompilationError.UnknownError
+				}
+			} catch CompilationError.Error(let message) {
+				return Response(status: .Error, text: message)
+			} catch {
+				return Response(status: .Error, text: "\(error)")
 			}
-		} catch CompilationError.Error(let message) {
-			return Response(status: .Error, text: message)
-		} catch {
-			return Response(status: .Error, text: "\(error)")
 		}
+
+		// TODO: Support caching headers/etags
+
+		return response
 	}
 
 	private func info(request: Request) -> RequestInfo {
